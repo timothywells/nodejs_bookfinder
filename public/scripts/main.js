7d139855dc9150d613ba1ec5b706e08e007d7eec
Timothy Wells <mr.timothywells@gmail.com>
2020-12-18 06:30:58 -0700

    20.12.18.01
    Almost done with this, just need to push to Heroku and test that it all functions as intended.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -19,7 +19,7 @@ function bookSearch(){
     } else {
         $.ajax({
             type:"POST",
-            data: {search:search, select:select},
+            data: {search:search, select:select + ":"},
             url: "/bookcall",
             dataType: "text",
             success: forLoop,
@@ -34,32 +34,45 @@ function bookSearch(){
   function forLoop(inputdata) {
     var data = JSON.parse(inputdata)
     console.log(data)
-    for(i = 0; i <= data.items.length; i++) {
+    var i;
+    var i;
+    for(i in data.items) {
         let result = ""
         result += "<div class=bookCard>"
             result += "<div class=row>"
-                result += "<a href=" + (data.items[i].volumeInfo.infoLink || '') + "><img class=bookCover src=" + (data.items[i].volumeInfo.imageLinks.smallThumbnail || 'Image Unavailable') + "/></a>"
-                    result += "<div class=bookDetails>"
-                        result += "<h3>" + (data.items[i].volumeInfo.title || 'Title Unavailable') + "</h3>"
-                            result += "<h4>Author(s):</h4>"
-                                result += "<ul>" 
-                                    data.items[i].volumeInfo.authors.forEach(author => {result += "<li>" + (author || 'Author Information Unavailable') + "</li>" })
-                                result += "</ul>"
-                            result += "<p id=description>" + (data.items[i].volumeInfo.description || 'Description Unavailable') + "</p>"
+                result += "<a href=" + (data.items[i].volumeInfo.infoLink || '') + ">"
+                    try {
+                        try {
+                            result += "<img class=bookCover src=" + data.items[i].volumeInfo.imageLinks.thumbnail + "/>"
+                        } catch {
+                            result += "<img class=bookCover src=" + data.items[i].volumeInfo.imageLinks.smallThumbnail + "/>"
+                        }
+                    } catch {
+                        result += "<img class=bookCover src=/media/nobook.jpg/>"
+                    }
+                result += "</a>"
+                result += "<div class=bookDetails>"
+                    result += "<h3 id=title>" + (data.items[i].volumeInfo.title || 'Title Unavailable') + "</h3>"
+                        result += "<h4>Author(s):</h4>"
+                            result += "<ul>"
+                                try {
+                                    data.items[i].volumeInfo.authors.forEach(author => {result += "<li>" + (author) + "</li>"})
+                                } catch {
+                                    result += "<li>Author Unavailable</li>"
+                                }
+                            result += "</ul>"
+                        result += "<p id=description>" + (data.items[i].volumeInfo.description || 'Description Unavailable') + "</p>"
                     result += "</div>"
-            result += "</div>"
+                result += "</div>"
             result += "<div class=links>"
                 result += "<div>"
-                    //result += '<script type=text/javascript src=https://books.google.com/books/previewlib.js></script>'
                     result += '<span id=GBS_Button0><img src=https://books.google.com/intl/en/googlebooks/images/gbs_preview_button1.gif border=0></span>'
-                    //result += '<script type=text/javascript>GBS_insertPreviewButtonPopup(\'ISBN:' + data.items[i].volumeInfo.industryIdentifiers[0].identifier + '\');</script>'                        
                 result += "</div>"
                 result += "<div class=linkbtn><a href=" + (data.items[i].volumeInfo.previewLink || alert("Preview Unavailable")) + " target=_blank>Preview</a></div>"
                 result += "<div class=linkbtn><a href=" + (data.items[i].volumeInfo.infoLink || alert("Preview Unavailable")) + " target=_blank>More Info</a></div>"
-                //result += "<div class=linkbtn><a href=" + (data.items[i].volumeInfo.canonicalVolumeLink || alert("Preview Unavailable")) + " target=_blank>Preview</a></div>"
             result += "</div>"        
         result += "</div>"
     books.innerHTML += result
     }      
-  }
+}
   
